8a8590e1206d254a55b6ef4af0ff2e7edc16a1d7
Egor Udovenko <udovenkoea@gmail.com>
2021-01-17 20:21:20 +0500

    Initial commit

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Егор on 19.12.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
